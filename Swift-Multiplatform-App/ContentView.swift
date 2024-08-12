05532b3cbe9ba3f9f66b4d7317d4999469080b13
zhihaofans <zhihaofans@hotmail.com>
2024-08-12 23:58:42 +0800

    Initial Commit

---
+++ Swift-Multiplatform-App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Swift-Multiplatform-App
+//
+//  Created by zzh on 2024/8/12.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Swift-Multiplatform-App/Swift_Multiplatform_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Swift_Multiplatform_AppApp.swift
+//  Swift-Multiplatform-App
+//
+//  Created by zzh on 2024/8/12.
+//
+
+import SwiftUI
+
+@main
+struct Swift_Multiplatform_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
